0742be321e615e1f05c3d2a010e170479a51546e
Violet <vi@violetro.se>
2026-03-14 02:58:09 -0400

    feat: additional pages and navigation

--- _includes/layouts/sketches.11ty.js
+++ _includes/layouts/sketches.11ty.js
@@ -3,6 +3,7 @@ import fs from "node:fs/promises"
 
 export const data = {
     layout: "layouts/base",
+    tags: "sketches",
 };
 export async function render(data) {
     // the file should be something like ./content/art/sketches/2026/02/25/index.md
@@ -11,6 +12,7 @@ export async function render(data) {
     // - ./content/art/sketches/2026/02/25/media/#.md
 
     const { dir } = path.parse(data.page.inputPath);
+
     const mediaFiles = {};
     for (const name of await fs.readdir(`${dir}/media`)) {
         const key = name.charAt(0);
@@ -24,8 +26,27 @@ export async function render(data) {
             mediaFiles[key]["img"] = `media/${name}`;
         }
     }
-    
-    return `${data.content}
+
+    // find the prev and next pages if any exist
+    let prev = null;
+    let next = null;
+    let allFiles = [];
+    for await (const entry of fs.glob(`${dir}/../../../*/*/*`)) {
+        allFiles.push(entry);
+    }
+    allFiles.sort();
+    let index = allFiles.findIndex(file => path.relative(file, dir) === "");
+    if (index > 0) {
+        prev = allFiles[index - 1].replace("content", "").replace("\\", "/");
+    }
+    if (index < allFiles.length - 1) {
+        next = allFiles[index + 1].replace("content", "").replace("\\", "/");
+    }
+    return (
+`<div class="container--lg">
+    <h1>${data.title}</h1>
+    <p><small>${prev === null ? "" : `<a href="${prev}">Previous</a>`} ${next === null ? "" : `<a href="${next}">Next</a>`}</small></p>
+    ${data.content}
     ${await Promise.all(Object.values(mediaFiles).map(async ({md, img}) => 
         `<figure>
             <img src="${img}" alt="" width="512">
@@ -33,5 +54,7 @@ export async function render(data) {
                 ${await this.renderFile(md)}
             </figcaption>` : ""}
         </figure>`
-    ))}`;
+    ))}
+</div>`
+    );
 }
---
+++ content/art/sketches/sketches.11tydata.js
@@ -0,0 +1,12 @@
+import path from "node:path"
+
+export default {
+    layout: "layouts/sketches",
+    eleventyComputed: {
+        title: (data) => {
+            const { dir } = path.parse(data.page.inputPath);
+            const date = new Date(dir.match(/\d\d\d\d\/\d\d\/\d\d/)[0]);
+            return date.toLocaleString("en-US", { weekday: "long", day: "numeric", month: "long", year: "numeric" });
+        }
+    }
+}
